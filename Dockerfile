c84517c6039d4bfec6c03b2bc2ae0b8df035a195
Gokul Methencheri <mgokulm@gmail.com>
2024-02-14 14:07:33 -0500

    Included version for dev-packages in Pipfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM ubuntu
 MAINTAINER Gokul Methencheri <mgokulm@gmail.com>
 
 RUN apt-get update
-RUN apt-get install -y python3-pip python3.11
+RUN apt-get install -y python3-pip python3.7
 RUN pip3 install --upgrade pip
 RUN pip3 install pipenv
 
--- tests/test_master_referencedata.py
+++ tests/test_master_referencedata.py
@@ -7,12 +7,12 @@ spark = conftest.setUp()
 
 def test_master_referencedata():
 
-    test_df1_raw = (master_referencedata_extract(spark,
-                                                 conftest.TEST_FILE_PATH + '/' + 'Policy_Input_data.csv'))
+    test_df1_raw = (master_referencedata_extract
+                    (spark, conftest.TEST_FILE_PATH + '/' + 'Policy_Input_data.csv'))
     test_df1_transformed = (master_referencedata_transform(test_df1_raw))
 
-    expected_df1_raw = (master_referencedata_extract(spark,
-                                                     conftest.TEST_FILE_PATH + '/' + 'Policy_Input_data.csv'))
+    expected_df1_raw = (master_referencedata_extract
+                        (spark, conftest.TEST_FILE_PATH + '/' + 'Policy_Output_data.csv'))
     expected_df1_transformed = (master_referencedata_transform(expected_df1_raw))
 
     assert test_df1_transformed.collect() == expected_df1_transformed.collect()
