c77b3ba0c05731bcbca413ca03e60a85241c8aca
Gokul Methencheri <mgokulm@gmail.com>
2024-02-14 01:26:11 -0500

    Initial commit

--- tests/test_master_referencedata.py
+++ tests/test_master_referencedata.py
@@ -2,34 +2,26 @@ import os
 
 import conftest
 import jobs.master_referencedata
-# from jobs.master_referencedata import *
+from jobs.master_referencedata import *
 
 spark = conftest.setUp()
 
 def test_master_referencedata():
     print("gkl inside test_master_referencedata")
 
-    test_df1_raw = (jobs.master_referencedata.
-               master_referencedata_extract
-               (spark, os.getcwd() + '/' + 'Policy_Input_data.csv'))
+    test_df1_raw = (master_referencedata_extract(spark,'Policy_Input_data.csv'))
+    test_df1_transformed = (master_referencedata_transform(test_df1_raw))
 
-    test_df1_transformed = (jobs.master_referencedata.
-                            master_referencedata_transform(test_df1_raw))
-
-
-    expected_df1_raw = (jobs.master_referencedata.
-               master_referencedata_extract
-               (spark, os.getcwd() + '/' + 'Policy_Input_data.csv'))
-
-    expected_df1_transformed = (jobs.master_referencedata.
-                            master_referencedata_transform(expected_df1_raw))
+    expected_df1_raw = (master_referencedata_extract(spark,'Policy_Input_data.csv'))
+    expected_df1_transformed = (master_referencedata_transform(expected_df1_raw))
 
     assert test_df1_transformed.collect() == expected_df1_transformed.collect()
+    return None
 
 conftest.tearDown(spark)
 
 #     return (test_df1_transformed)
-#
+# #
 # if __name__ == '__main__':
 #     test_df = test_master_referencedata()
 #     print(test_df.show())
